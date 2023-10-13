9567c3b19e8b90551e8a3b81a5a9b557c0239664
xCHZx <chzx94@gmail.com>
2023-10-13 00:04:15 -0500

    Create dialog/modal component for display the commit info

--- src/components/CommitList.vue
+++ src/components/CommitList.vue
@@ -1,25 +1,28 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios';
-import { getCommits } from '../services/Octokit'
+import { getCommits, getSingleCommit } from '../services/Octokit'
 
 onMounted(() => {
   getData()
 })
 
 const commits = ref();
+const singleCommit = ref();
+const dialog = ref(false);
 
 const getData = async () => {
   commits.value = await getCommits()
 }
 
+const getCommitData = async (sha: String) => {
+  console.log(sha);
+  singleCommit.value = await getSingleCommit(sha)
+}
+
 const convertDate = (rawDate: Date) => {
   const date = new Date(rawDate)
   return date
-}
-
-const countFiles = (files: Object) => {
-  console.log(files)
 }
 
 </script>
@@ -40,18 +43,51 @@ const countFiles = (files: Object) => {
             {{ commit.commit.message }}
           </v-card-title>
           <v-card-text>
-            <div>{{ countFiles(commit) }} Archivos afectados</div>
-            <a target="blank" :href="commit.html_url"> Ver en GitHub</a>
+            <a target="blank" :href="commit.html_url"> See commit in Github</a>
             <div>
-              <v-btn variant="tonal" size="small" class="mt-3">
-                Más información
+              <v-btn variant="tonal" size="small" class="mt-3" @click="dialog = true, getCommitData(commit.sha)">
+                More Information
               </v-btn>
             </div>
-
-
           </v-card-text>
         </v-card>
       </v-timeline-item>
     </v-timeline>
   </div>
+
+  <v-dialog v-model="dialog" width="800" v-if="singleCommit">
+    <v-card class="pt-5 pl-5 pr-5">
+      <v-card-title>
+        <!-- {{ singleCommit ? singleCommit.commit.message : 'Loading...' }} -->
+        <h3><b>{{ singleCommit.commit.message }}</b></h3>
+        <h6>Created by {{ singleCommit.commit.author.name }} @ {{ convertDate(singleCommit.commit.author.date) }}</h6>
+        <h6 style="font-style:italic">#{{ singleCommit.sha }}<a target="blank" :href="singleCommit.html_url"> See commit
+            in Github</a></h6>
+      </v-card-title>
+      <v-card-text>
+        <b>Files Affected:</b> {{ Object.keys(singleCommit.files).length }}
+        <v-expansion-panels class="mt-4">
+          <v-expansion-panel style="width:min-content;" v-for="file in singleCommit.files" :key="file">
+            <v-expansion-panel-title>📄{{ file.filename }}</v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-row>
+                <v-col cols="3">
+                  <b>✅Additions:</b> {{ file.additions }}
+                </v-col>
+                <v-col cols="3">
+                  <b>🗑️Deletions:</b> {{ file.deletions }}
+                </v-col>
+                <v-col cols="3">
+                  <b>🔨Changes:</b> {{ file.changes }}
+                </v-col>
+              </v-row>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" block @click="dialog = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
--- src/services/Octokit.ts
+++ src/services/Octokit.ts
@@ -1,15 +1,22 @@
 
 import { Octokit } from "octokit";
 
+
 const owner = "xCHZx";
 const repo = "ftf-tht-frontend";
 
 const octokit = new Octokit({
-    auth: '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',
-});
+    auth: import.meta.env.VITE_OCTOKIT_TOKEN
+})
 
 export const getCommits = async () => {
     const result = await octokit.request(`GET /repos/${owner}/${repo}/commits`);
+    // console.log(result.data);
+    return result.data;
+}
+
+export const getSingleCommit = async (sha: String) => {
+    const result = await octokit.request(`GET /repos/${owner}/${repo}/commits/${sha}`);
     console.log(result.data);
     return result.data;
 }
